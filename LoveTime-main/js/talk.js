a0e58416e8b5e2ff8f47b9e0b1f020b2e2899919
SuIGuAnG1 <119528037+SuIGuAnG1@users.noreply.github.com>
2022-12-01 02:04:55 +0800

    Add files via upload

---
+++ LoveTime-main/js/talk.js
@@ -0,0 +1,99 @@
+new TypeIt("#lywry", {
+        loop: false,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("YX && ZYQ")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("爱你宝贝")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("2022.7.15")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我们第一次在光遇相遇")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("五十天的陪伴")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("10月3号你同意了我谈恋爱的追求")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("到现在已经59天了")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我们互相陪伴彼此度过了自己的十八岁和二十岁生日")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我会不小心惹你生气")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("你也会害怕控制不住自己凶我而故意不理我")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("但是我们对彼此做出了承诺")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我们不会离开对方")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("不论怎样")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我永远爱❤❤❤❤❤❤❤你")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("❤感谢相遇❤")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("❤感谢有你的陪伴❤")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("❤很期待与赵女士共同步入婚姻的殿堂❤")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    }).type("❤爱你哟，宝❤宝，感谢观看哟❤")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToZYQ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
